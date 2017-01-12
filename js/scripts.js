0c2dfce5225466a0552396ae4ffd6bbcedbfeb87
margrahaw <margrahaw@gmail.com>
2017-01-12 16:01:06 -0500

    Update two
    finished the javascript for the alert notification

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 window.onload = function() {
 	//Buttons
 	var alertButton = document.getElementById("closebtn");
+	var alertBanner	= document.getElementById("alertbtn");
+
+	//Add Event listener for alert
+	alertButton.addEventListener('click', () => {
+
+		alertBanner.className = 'hide';
+	})
+
+	
 }
