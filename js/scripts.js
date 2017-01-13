714e17082accfc216eb618373ce47f4c5df8c6ad
margrahaw <margrahaw@gmail.com>
2017-01-12 22:52:29 -0500

    Edit Three
    Added Daily Traffic Widget

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,42 @@ window.onload = function() {
 	var alertButton = document.getElementById("closebtn");
 	var alertBanner	= document.getElementById("alertbtn");
 
+	//Charts
+	var dailyTrafficChart = document.getElementById("dailyTrafficChart");
+
 	//Add Event listener for alert
 	alertButton.addEventListener('click', () => {
-
 		alertBanner.className = 'hide';
 	})
 
+	var myChart = new Chart(dailyTrafficChart, {
+    type: 'bar',
+    data: {
+        labels: ["S", "M", "T", "W", "T", "F", "S"],
+        datasets: [{
+            // label: 'Daily Traffic',
+            data: [75, 100, 275, 125, 225, 200, 100],
+            backgroundColor: 'rgba(116, 119, 191, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        },
+        title: {
+        	display: true,
+        	text: 'Daily Traffic'
+        },
+        legend: {
+        	display: false
+        },
+        padding: '10'
+    }
+});
 	
 }
